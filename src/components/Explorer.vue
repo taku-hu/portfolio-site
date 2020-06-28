ea23c7ef754a2d379bc50a9210e38818dbc09d65
taku-hu <hs.tm.ec.a.tt@gmail.com>
2020-06-28 23:30:10 +0900

    fix: router-linkを関数から発火するよう変更＆アコーディオンのコードを纏めた

--- src/components/Explorer.vue
+++ src/components/Explorer.vue
@@ -24,42 +24,13 @@
       <div class="accordion__body" v-show="accordion.isOpen">
         <a
           class="accordion__links"
-          :href="path.path"
-          target="_blank"
           v-for="path in accordion.paths"
           :key="path.name"
+          @click="transitionPage(accordion, path)"
         >
           <span v-html="path.icon"></span>
           {{ path.name }}
         </a>
-      </div>
-    </div>
-
-    <div class="accordion">
-      <div
-        class="accordion__label"
-        :class="{
-          'accordion__label--opened': state.links.isOpen,
-          'accordion__label--theme-changed': isThemeChanged
-        }"
-        @click="togglePages"
-      >
-        <i
-          class="accordion__icon fas fa-angle-down"
-          :class="{ 'accordion__icon--close': !state.links.isOpen }"
-        ></i>
-        {{ state.links.labelName }}
-      </div>
-      <div class="accordion__body" v-show="state.links.isOpen">
-        <router-link
-          class="accordion__links"
-          v-for="path in state.links.paths"
-          :key="path.name"
-          :to="path.path"
-        >
-          <span v-html="path.icon"></span>
-          {{ `${path.name.substring(0, 1)}${path.name.substring(1).toLowerCase()}` }}
-        </router-link>
       </div>
     </div>
   </div>
@@ -67,6 +38,7 @@
 
 <script>
 import { defineComponent, reactive } from 'vue';
+import router from '@/router';
 
 export default defineComponent({
   props: {
@@ -82,7 +54,7 @@ export default defineComponent({
             {
               icon: '<i class="fab fa-vuejs"></i>',
               name: 'My portfolio Site',
-              path: 'https://github.com/taku-hu/portfolio-site'
+              link: 'https://github.com/taku-hu/portfolio-site'
             }
           ]
         },
@@ -93,37 +65,37 @@ export default defineComponent({
             {
               icon: '<i class="fab fa-quora"></i>',
               name: 'Qiita',
-              path: 'https://qiita.com/taku-hu'
+              link: 'https://qiita.com/taku-hu'
+            }
+          ]
+        },
+        {
+          labelName: 'pages',
+          isOpen: true,
+          paths: [
+            {
+              icon: '<i class="fas fa-home">',
+              name: 'Home',
+              link: '/'
+            },
+            {
+              icon: '<i class="fas fa-user-circle">',
+              name: 'About',
+              link: '/about'
+            },
+            {
+              icon: '<i class="fas fa-wrench"></i>',
+              name: 'Skills',
+              link: '/skills'
+            },
+            {
+              icon: '<i class="far fa-address-card"></i>',
+              name: 'Works',
+              link: '/works'
             }
           ]
         }
-      ],
-      links: {
-        labelName: 'pages',
-        isOpen: true,
-        paths: [
-          {
-            icon: '<i class="fas fa-home">',
-            name: 'HOME',
-            path: '/'
-          },
-          {
-            icon: '<i class="fas fa-user-circle">',
-            name: 'ABOUT',
-            path: '/about'
-          },
-          {
-            icon: '<i class="fas fa-wrench"></i>',
-            name: 'SKILLS',
-            path: '/skills'
-          },
-          {
-            icon: '<i class="far fa-address-card"></i>',
-            name: 'WORKS',
-            path: '/works'
-          }
-        ]
-      }
+      ]
     });
 
     const toggleAccordion = accordion => {
@@ -132,14 +104,19 @@ export default defineComponent({
       );
       selected.isOpen = !selected.isOpen;
     };
-    const togglePages = () => {
-      state.links.isOpen = !state.links.isOpen;
-    };
+
+    const transitionPage = (accordion, path) => {
+      if(accordion.labelName === 'pages') {
+        router.push({ path: path.link });
+      } else {
+        open(path.link, '_blank');
+      }
+    }
 
     return {
       state,
       toggleAccordion,
-      togglePages
+      transitionPage
     };
   }
 });
@@ -216,6 +193,7 @@ export default defineComponent({
       @include button-sizing;
       display: block;
       box-sizing: border-box;
+      cursor: pointer;
       padding-left: 1.5rem;
       &:hover {
         background-color: #313341;
